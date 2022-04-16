f545495ba05937411bd0b8fc311f0215ac4b0899
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-04-16 17:07:57 -0600

    adds elephantSLQ functionality

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const Express = require('express');
 const App = Express();
+const pg = require('pg');
+const Client = pg.Client;
 const BodyParser = require('body-parser');
 const PORT = 8080;
 
@@ -17,3 +20,22 @@ App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
+
+const configObj = {
+  host: process.env.ELE_HOST,
+  user: process.env.ELE_USER,
+  password: process.env.ELE_PW,
+  database: process.env.ELE_DB,
+  port: 5432
+};
+
+const client = new Client(configObj);
+
+client.connect();
+
+/// Used below query to test that it was working
+
+client.query(`SELECT * FROM users;`)
+  .then((res) => {
+    console.log(res.rows);
+  });
