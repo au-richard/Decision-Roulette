97b37da74993f6ec0e13cf0aa66550489e4891a0
Richard <au.richard4@gmail.com>
2022-04-18 19:53:53 -0700

    Adding material ui react drawer.

--- react-front-end/src/components/Navigation.js
+++ react-front-end/src/components/Navigation.js
@@ -1,61 +1,221 @@
-import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
-import '../styles/Navigation.scss';
+// import React from 'react';
+// import { slide as Menu } from 'react-burger-menu';
+// import '../styles/Navigation.scss';
 
-class Navigation extends React.Component {
-    showSettings (event) {
-    event.preventDefault();
-  }
-
-
-  render () {
-    return (
-      <Menu>
-        <h3>Logged in as User | <a href="">Logout</a></h3>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Login</a>
-        <a id="contact" className="menu-item" href="/contact">Sign Up</a>
-      </Menu>
-    );
-  }
-}
-
-export default Navigation;
-// class Example extends React.Component {
-//   showSettings (event) {
-//     event.preventDefault();
-//     .
-//     .
-//     .
+// const Navigation = () => {
+//   const showSettings = (event) => {
+//   event.preventDefault();
 //   }
 
-//   render () {
-//     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+
 //     return (
 //       <Menu>
+//         <h3>Logged in as User | <a href="">Logout</a></h3>
 //         <a id="home" className="menu-item" href="/">Home</a>
 //         <a id="about" className="menu-item" href="/about">About</a>
-//         <a id="contact" className="menu-item" href="/contact">Contact</a>
-//         <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+//         <a id="contact" className="menu-item" href="/contact">Login</a>
+//         <a id="contact" className="menu-item" href="/contact">Sign Up</a>
 //       </Menu>
 //     );
 //   }
-// }
 
-// function Navigation() {
+// export default Navigation;
+
+
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import { Routes, Route, Link } from "react-router-dom";
+import '../styles/Navigation.scss';
+
+function Navigation(props) {
+  const drawerWidth = 240;
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  
+  return (
+    <div class="sidebar">
+            <div class="sidebar-icon">
+
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        edge="start"
+        onClick={handleDrawerToggle}
+        sx={{ mr: 2, display: { sm: 'none' } }}
+      >
+        <MenuIcon />
+
+      </IconButton>
+      </div>
+
+      <Drawer
+        // container={container}
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        sx={{
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        <h3>Logged in as User</h3>
+        <Divider />
+        <List>
+          <ListItem component={Link} to="/">
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem component={Link} to="/about">
+            <ListItemText primary="About" />
+          </ListItem>
+          <ListItem component={Link} to="/create">
+            <ListItemText primary="Create Category" />
+          </ListItem>
+          <ListItem component={Link} to="/edit">
+            <ListItemText primary="Edit Category" />
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem component={Link} to="/login">
+            <ListItemText primary="Login" />
+          </ListItem>
+          <ListItem component={Link} to="/signup">
+            <ListItemText primary="Sign Up" />
+          </ListItem>
+          <ListItem component={Link} to="/logout">
+            <ListItemText primary="Logout" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
+}
+
+export default Navigation;
+
+
+
+
+
+// const drawerWidth = 240;
+
+// function Navigation(props) {
+//   const { window } = props;
+//   const [mobileOpen, setMobileOpen] = React.useState(false);
+
+//   const handleDrawerToggle = () => {
+//     setMobileOpen(!mobileOpen);
+//   };
+
+//   const drawer = (
+//     <div>
+//       <Toolbar />
+//       <Divider />
+//       <List>
+//         {['Home', 'About', 'Create Category', 'Edit Category'].map((text, index) => (
+//           <ListItem button key={text}>
+//             <ListItemText primary={text} />
+//           </ListItem>
+//         ))}
+//       </List>
+//       <Divider />
+//       <List>
+//         {['Login', 'Signup'].map((text, index) => (
+//           <ListItem button key={text}>
+//             <ListItemText primary={text} />
+//           </ListItem>
+//         ))}
+//       </List>
+//     </div>
+//   );
+
+//   const container = window !== undefined ? () => window().document.body : undefined;
+
 //   return (
-//     <nav>
-//       <span className="nav--text">tweeter</span>
-
-//       <div className="btn btn--scroll">
-//         <a href="#" className="btn__text btn--borderless" title="Compose Tweet">
-//           <span className="btn--bold">Write</span> a new tweet
-//           <br />
-//             <i className="btn__icon fas fa-angle-double-down"></i>
-//         </a>  
-//       </div>  
-//     </nav>
+//     <Box sx={{ display: 'flex' }}>
+//       <CssBaseline />
+//       <AppBar
+//         position="fixed"
+//         sx={{
+//           width: { sm: `calc(100% - ${drawerWidth}px)` },
+//           ml: { sm: `${drawerWidth}px` },
+//         }}
+//       >
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             edge="start"
+//             onClick={handleDrawerToggle}
+//             sx={{ mr: 2, display: { sm: 'none' } }}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           {/* <Typography variant="h6" noWrap component="div">
+//             Responsive drawer
+//           </Typography> */}
+//         </Toolbar>
+//       </AppBar>
+//       <Box
+//         component="nav"
+//         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+//         aria-label="mailbox folders"
+//       >
+//         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+//         <Drawer
+//           container={container}
+//           variant="temporary"
+//           open={mobileOpen}
+//           onClose={handleDrawerToggle}
+//           ModalProps={{
+//             keepMounted: true, // Better open performance on mobile.
+//           }}
+//           sx={{
+//             display: { xs: 'block', sm: 'none' },
+//             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+//           }}
+//         >
+//           {drawer}
+//         </Drawer>
+//         <Drawer
+//           variant="permanent"
+//           sx={{
+//             display: { xs: 'none', sm: 'block' },
+//             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+//           }}
+//           open
+//         >
+//           {drawer}
+//         </Drawer>
+//       </Box>
+//       <Box
+//         component="main"
+//         sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+//       >
+//         <Toolbar />
+//       </Box>
+//     </Box>
 //   );
 // }
 
+// Navigation.propTypes = {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   window: PropTypes.func,
+// };
+
+// export default Navigation;
