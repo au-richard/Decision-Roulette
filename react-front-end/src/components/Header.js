21e40795683bb7f89fc1f1a684e7253f5dc59d15
Richard <au.richard4@gmail.com>
2022-04-21 11:56:40 -0700

    Adding search query code.

--- react-front-end/src/components/Header.js
+++ react-front-end/src/components/Header.js
@@ -30,44 +30,46 @@ class Header extends Component {
   render() {
 // export default function Header() {
 
-  const googleSearch = () => {
-    const options = {
-      method: 'GET',
-      url: 'https://google-search1.p.rapidapi.com/google-search',
-      params: {hl: 'en', q: 'Cats', gl: 'ca'},
-      headers: {
-        'X-RapidAPI-Host': 'google-search1.p.rapidapi.com',
-        'X-RapidAPI-Key': '6d4e909f3fmsh77e9d4b7752328cp19fdc9jsnda324b19f180'
-      }
-    };
-    axios.request(options).then(function (response) {
-      console.log(response.data.organic[0]);
-    }).catch(function (error) {
-      console.error(error);
-    });
-  }
+  // const googleSearch = () => {
+  //   const options = {
+  //     method: 'GET',
+  //     url: 'https://google-search1.p.rapidapi.com/google-search',
+  //     params: {hl: 'en', q: 'Cats', gl: 'ca'},
+  //     headers: {
+  //       'X-RapidAPI-Host': 'google-search1.p.rapidapi.com',
+  //       'X-RapidAPI-Key': '6d4e909f3fmsh77e9d4b7752328cp19fdc9jsnda324b19f180'
+  //     }
+  //   };
+  //   axios.request(options).then(function (response) {
+  //     console.log(response.data.organic[0]);
+  //   }).catch(function (error) {
+  //     console.error(error);
+  //   });
+  // }
+  //   return (
+  //   <section className="header">
+  //   <button type="submit" id="new-item" onClick={googleSearch}>New</button>
+  //   <h4 className="header_message">Decision Roulette</h4>
+  //   </section>
+  //   );
     return (
-    <section className="header">
-    <button type="submit" id="new-item" onClick={googleSearch}>New</button>
-    <h4 className="header_message">Decision Roulette</h4>
-    </section>
+    <>
+    {/* changing font letter colors */}
+    {/* <font color="orange"></font> */}
+    <h4 className="header_message">De<font color="lightblue">c</font>ision Roulette</h4>
+    <div className="category">
+    <form className='category_form' method='get' action='/'>
+      <input type="text" id="form" placeholder='Enter Category'></input>
+    </form>
+    <div className="icon">
+    <button className="icon_button" onClick={this.fetchData}> 
+    <FontAwesomeIcon icon={faForward} />
+    </button>
+    </div>
+    </div>
+    </>
     );
-    // return (
-    // <>
-    // {/* changing font letter colors */}
-    // {/* <font color="orange"></font> */}
-    // <h4 className="header_message">De<font color="lightblue">c</font>ision Roulette</h4>
-    // <div className="category">
-    // <form className='category_form' method='get' action='/'>
-    //   <input type="text" id="form" placeholder='Enter Category'></input>
-    // </form>
-    // <div className="icon">
-    // <button className="icon_button" onClick={this.fetchData}> 
-    // <FontAwesomeIcon icon={faForward} />
-    // </button>
-    // </div>
-    // </div>
-    // </>
-    // );
   }
 }
+
+export default Header;
