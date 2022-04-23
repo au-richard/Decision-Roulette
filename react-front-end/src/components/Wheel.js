3c65bd74c0c07a0a0a384f66b854317454f85659
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-04-22 18:10:28 -0600

    adjustments after merge

--- react-front-end/src/components/Wheel.js
+++ react-front-end/src/components/Wheel.js
@@ -7,13 +7,7 @@ import { searchContext } from '../providers/SearchProvider';
 // import { Howl } from 'howler';
 
 // loop through fetchResult to obtain data for "option" key in data array below
-let data = [
-  { option: "option 1", style: {backgroundColor: '#170055', textColor: 'azure', } }
-  // { option: (ctx.data.organic[1].title || "option 1"), style: {backgroundColor: '#3E00FF', textColor: 'orange'} },
-  // { option: (ctx.data.organic[2].title || "option 1"), style: {backgroundColor: '#DC143C', textColor: '#7FFF00'} },
-  // { option: (ctx.data.organic[3].title || "option 1"), style: {backgroundColor: '#B5FFD9', textColor: '#808080'} },
-  // { option: (ctx.data.organic[4].title || "option 1"), style: {backgroundColor: 'purple', textColor: 'orange'} },
-  // { option: (ctx.data.organic[5].title || "option 1"), style: {backgroundColor: '#7FFF00', textColor: '#3E00FF'} }
+
 import { Howl } from "howler";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faForward } from '@fortawesome/free-solid-svg-icons'
@@ -32,14 +26,14 @@ const data = [
 export default () => {
   const ctx = useContext(searchContext)
   console.log(ctx.data);
-  data = [
-    { option: (ctx.data.organic[0].title || "option 1"), style: {backgroundColor: '#170055', textColor: 'azure', } },
-    { option: (ctx.data.organic[1].title || "option 1"), style: {backgroundColor: '#3E00FF', textColor: 'orange'} },
-    { option: (ctx.data.organic[2].title || "option 1"), style: {backgroundColor: '#DC143C', textColor: '#7FFF00'} },
-    { option: (ctx.data.organic[3].title || "option 1"), style: {backgroundColor: '#B5FFD9', textColor: '#808080'} },
-    { option: (ctx.data.organic[4].title || "option 1"), style: {backgroundColor: 'purple', textColor: 'orange'} },
-    { option: (ctx.data.organic[5].title || "option 1"), style: {backgroundColor: '#7FFF00', textColor: '#3E00FF'} }
-  ]
+  // data = [
+  //   { option: (ctx.data.organic[0].title || "option 1"), style: {backgroundColor: '#170055', textColor: 'azure', } },
+  //   { option: (ctx.data.organic[1].title || "option 1"), style: {backgroundColor: '#3E00FF', textColor: 'orange'} },
+  //   { option: (ctx.data.organic[2].title || "option 1"), style: {backgroundColor: '#DC143C', textColor: '#7FFF00'} },
+  //   { option: (ctx.data.organic[3].title || "option 1"), style: {backgroundColor: '#B5FFD9', textColor: '#808080'} },
+  //   { option: (ctx.data.organic[4].title || "option 1"), style: {backgroundColor: 'purple', textColor: 'orange'} },
+  //   { option: (ctx.data.organic[5].title || "option 1"), style: {backgroundColor: '#7FFF00', textColor: '#3E00FF'} }
+  // ]
 
   const [mustSpin, setMustSpin] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
@@ -75,7 +69,7 @@ export default () => {
 
   return (
     <>
-      <div className="category">
+      {/* <div className="category">
         <form className='category_form' method='get' action='/'>
           <input type="text" id="form" placeholder='Enter Category'></input>
         </form>
@@ -84,7 +78,7 @@ export default () => {
             <FontAwesomeIcon icon={faForward} />
           </button>
         </div>
-      </div>
+      </div> */}
       <button onClick={handleSpinClick} className='spin_button'>SPIN</button>
       <Wheel
         mustStartSpinning={mustSpin}
